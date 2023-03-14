93020bfe42a607010e2e4a81ae262609199c3a04
lucky7xz <ml.pojoni42@outlook.com>
2023-03-14 13:40:47 +0200

    text file eq split done, testing done. added promt source file

--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,66 @@
 def maybe():
     return 1
+
+
+
+import math
+import random
+import glob
+
+def split_text(text, max_length):
+    # Calculate the number of splits needed to create strings of roughly equal length
+    num_splits = math.ceil(len(text) / max_length)
+
+    # Calculate the length of each split
+    split_length = len(text) / (num_splits)
+
+    # Initialize variables to keep track of the current string and its length
+    current_str = ""
+    current_length = 0
+
+    # Initialize an empty list to store the resulting strings
+    result = []
+
+    # Loop through each character in the text
+    for i in range(len(text)):
+        # Add the current character to the current string
+        current_str += text[i]
+        current_length += 1
+
+        
+        if current_length >= split_length:
+            # check if space
+            if text[i] == " ":
+                # Add the current string to the result
+                result.append(current_str)
+                # Reset the current string and its length
+                current_str = ""
+                current_length = 0
+            
+            else:
+                # keep going
+                continue
+
+    return result            
+            
+
+
+
+
+# read the file
+
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        text = f.read()
+    return text
+
+
+
+text = read_file('data_in/small.en/borrowed_future_wav_text/RM3387691770.txt')
+splitz = split_text(text, 7000)
+
+#for result in splitz:
+   # print(result, "\n\n\n")
+
+#for result in splitz:
+ #   print(len(result))
--- testing.py
+++ testing.py
@@ -1,28 +1,83 @@
-def yep():
-    return "yep"
+
+import random
+import glob
+
+from preprocess import split_text, read_file
+
+def test_text_split(max_length, ): 
+
+    print("--- Testing text split function ---")
+
+    bf_samples = glob.glob('data_in/small.en/borrowed_future_wav_text/*.txt')
+    lex_samples = glob.glob('data_in/small.en/lex_fridman_podcast_text/*.txt')
+    SW_samples = glob.glob('data_in/small.en/science_weekly_text/*.txt')
+
+    # pick 5 random samples from each and add them to a list test_texts
+
+    test_texts = []
+
+    for i in range(5):
+        bf = random.choice(bf_samples)
+        lex = random.choice(lex_samples)
+        sw = random.choice(SW_samples)
+
+        test_texts.append(bf)
+        test_texts.append(lex)
+        test_texts.append(sw)
+
+    # read the text from each file and split it into strings of length max_length
+    # add the resulting strings to a list test_texts
+
+    splitz_to_test = []
+
+    for file in test_texts:
+
+        text = read_file(file)
+        splitz = split_text(text, max_length)
+
+        for result in splitz:
+            splitz_to_test.append(result)
+        
+    
+    # now we test
+
+    # a list comprehension to get the length of each string in splitz_to_test
+    lengths = [len(splitz_to_test[i]) for i in range(len(splitz_to_test))]
+
+    # list comprehension to check if each string in splitz_to_test is less than or equal to max_length
+    less_than_max = [len(splitz_to_test[i]) <= max_length for i in range(len(splitz_to_test))]
+
+    # list comprehension to check if each string is at least 75% of max_length
+    at_least_half = [len(splitz_to_test[i]) >= (max_length * 0.5) for i in range(len(splitz_to_test))]
+
+    
+    if all(less_than_max):
+        print("All strings are less than or equal to max_length")
+    
+    else:
+        print("Some strings are greater than max_length")
 
 
 
+    if all(at_least_half):
+        print("All strings are at least 50% of max_length")
+
+    else:
+        print("Some strings are less than 50% of max_length")
 
 
-'''
-
-What tests to run?
-
-
-intra : 
-
-
-output :
-
-trivial:
-
-- files not empty
-- 
-
-non-trivial:
-quotes exist in text
+    print("\n\nThe lengths of the strings are: ")
+    print(lengths)
+    print("\n\n")
 
 
 
-'''
+def test_text_split_visual(max_len):
+
+
+    pass
+
+
+########## Main ##########
+
+test_text_split(7000)
